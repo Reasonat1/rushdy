7c84a96eca144081f65272457463424e591f72be
rushdy@reasonat <rushdy@reasonat>
2016-06-23 14:39:39 +0200

    yakoub work

--- sites/all/themes/rushdi/templates/node--recipes.tpl.php
+++ sites/all/themes/rushdi/templates/node--recipes.tpl.php
@@ -8,23 +8,19 @@
  */
 ?>
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-​
   <?php if ($title_prefix || $title_suffix || $display_submitted || $unpublished || !$page && $title): ?>
     <header>
-
       <?php if ($display_submitted): ?>
         <p class="submitted">
           <?php print $user_picture; ?>
           <?php print $submitted; ?>
         </p>
       <?php endif; ?>
-​
       <?php if ($unpublished): ?>
         <mark class="unpublished"><?php print t('Unpublished'); ?></mark>
       <?php endif; ?>
     </header>
   <?php endif; ?>
-​
   <?php
     // We hide the comments and links now so that we can render them later.
     hide($content['comments']);
@@ -47,15 +43,28 @@
    </div>
    <div id="recipe-container-right">
      <div class="recipe-container-right-node-title">
-           <?php print render($title_prefix); ?>
-      <?php if (!$page && $title): ?>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
         <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
      </div>
-     <div class="recipe-conatiner-right-recipe-tags-icons">
-      <?php print render($content['field_recipe_category']); ?>
-     </div>
+     <?php $recipe_categories = field_get_items('node', $node, 'field_recipe_category'); ?>
+     <?php if ($recipe_categories) : ?>
+       <div class="recipe-conatiner-right-recipe-tags-icons">
+        <?php foreach ($recipe_categories as $category) : ?>
+          <?php 
+            $icon = field_get_items('taxonomy_term', $category['taxonomy_term'], 'field_category_icon');
+            if (empty($icon)) {
+              continue;
+            }
+            //print check_plain($category['taxonomy_term']->name);
+            $icon = field_view_field('taxonomy_term', $category['taxonomy_term'], 'field_category_icon', array('label' => 'hidden'));
+            print render($icon);
+          ?>
+        <?php endforeach; ?>
+       </div>
+     <?php endif; ?>
      <div class="recipe-container-right-servsnumber">
       <?php print render($content['field_number_of_servs']); ?>
      </div>
@@ -68,13 +77,9 @@
      <div class="recipe-container-right-preperation-steps">
       <?php print render($content['field_preparetion_steps']); ?>
      </div>
-​
    </div>
     
   </div>
-​
   <?php print render($content['links']); ?>
-​
   <?php print render($content['comments']); ?>
-​
 </article>
